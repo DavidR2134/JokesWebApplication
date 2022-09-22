1b2df202822cd4966e5eb0d5467e7a611ac97332
DavidR2134 <davidrogers2134@gmail.com>
2022-09-21 18:36:53 -0700

    Add project files.

---
+++ Data/Migrations/20220921140007_initialsetup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JokesApplication.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Jokes",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    JokeName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JokeAnswer = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jokes", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jokes");
+        }
+    }
+}
---
+++ Models/Jokes.cs
@@ -0,0 +1,14 @@
+﻿namespace JokesApplication.Models
+{
+    public class Jokes
+    {
+        public int id { get; set; }
+        public string JokeName { get; set; }
+        public string JokeAnswer { get; set; }
+
+        public Jokes()
+        {
+           
+        }
+    }
+}
